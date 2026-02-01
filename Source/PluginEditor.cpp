f3e667976edef180b81fb0ab5b303161db5f1d54
Alexander-Vassilev <alexander_vassilev@brown.edu>
2026-02-01 00:03:18 -0500

    button fixed

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -14,42 +14,39 @@ HackBrownAudioProcessorEditor::HackBrownAudioProcessorEditor (HackBrownAudioProc
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
     recordButton.setClickingTogglesState(true);
+    recordButton.setToggleState(false, juce::dontSendNotification);
     
     recordButton.onClick = [&]() {
         bool isOn = recordButton.getToggleState();
-        const auto message = recordButton.getToggleState() ? "Recording!" : "Record";
+        auto message = "Recording!";
+        if (!recordButton.getToggleState()) {
+            message = "Record";
+        }
+        //const auto message =  ? "Recording!" : "Record";
         recordButton.setButtonText(message);
         
         if (!isOn && p.recordingStarted.load()) {
             // Call Gabe FFT Algorithm Here
             // p.startRecording();
-            if (!isOn && p.recordingStarted.load())
-            {
-                // Call Gabe FFT Algorithm Here  --> do classification instead
-                p.inputProcessor.classifyStoredHits(p.getSampleRate());
-
-                recordButton.setButtonText("Recorded Thing");
-            }
+            // Call Gabe FFT Algorithm Here  --> do classification instead
+            p.inputProcessor.classifyStoredHits(p.getSampleRate());
+            recordButton.setButtonText("Recorded Thing");
+            addAndMakeVisible(playButton);
             
-            for (int i = 0; i < p.inputProcessor.storedHitsIndex; ++i)
-            {
+            for (int i = 0; i < p.inputProcessor.storedHitsIndex; ++i) {
                 const auto& hit = p.inputProcessor.storedHits[i];
 
                 DBG("Editor sees Hit #" << i
                     << " classified as "
                     << HitClassifier::toString(hit.type));
             }
-
-            recordButton.setButtonText("Recorded Thing");
-            
         }
         
-        p.recordingEnabled.store(!isOn);
+        p.recordingEnabled.store(isOn);
     };
     
     playButton.onClick = [&]() {
         p.isPlaybackOn.store(true);
-        recordButton.setButtonText("Clicked!");
         
         std::cout << p.inputProcessor.storedHitsIndex << std::endl;
         //p.inputProcessor.storedHits;
@@ -59,7 +56,6 @@ HackBrownAudioProcessorEditor::HackBrownAudioProcessorEditor (HackBrownAudioProc
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     addAndMakeVisible(recordButton);
-    addAndMakeVisible(playButton);
     setSize (400, 300);
 }
 
