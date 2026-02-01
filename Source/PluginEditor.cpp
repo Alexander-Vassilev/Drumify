13495001648725153d647dac3a73557c5a0fc537
Alexander-Vassilev <alexander_vassilev@brown.edu>
2026-02-01 00:07:22 -0500

    button fised

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -14,13 +14,19 @@ HackBrownAudioProcessorEditor::HackBrownAudioProcessorEditor (HackBrownAudioProc
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
     recordButton.setClickingTogglesState(true);
+    recordButton.setToggleState(false, juce::dontSendNotification);
     
     recordButton.onClick = [&]() {
         bool isOn = recordButton.getToggleState();
-        const auto message = recordButton.getToggleState() ? "Recording!" : "Record";
+        auto message = "Recording!";
+        if (!recordButton.getToggleState()) {
+            message = "Record";
+        }
+        //const auto message =  ? "Recording!" : "Record";
         recordButton.setButtonText(message);
         
         if (!isOn && p.recordingStarted.load()) {
+<<<<<<< HEAD
             // algorithm classifies/do classification
 
                     p.inputProcessor.classifyStoredHits(p.getSampleRate());
@@ -38,14 +44,74 @@ HackBrownAudioProcessorEditor::HackBrownAudioProcessorEditor (HackBrownAudioProc
 
             recordButton.setButtonText("Recorded Thing");
             
+=======
+            // Call Gabe FFT Algorithm Here
+            // p.startRecording();
+            // Call Gabe FFT Algorithm Here  --> do classification instead
+            p.inputProcessor.classifyStoredHits(p.getSampleRate());
+            recordButton.setButtonText("Recorded Thing");
+            addAndMakeVisible(playButton);
+            
+            for (int i = 0; i < p.inputProcessor.storedHitsIndex; ++i) {
+                const auto& hit = p.inputProcessor.storedHits[i];
+
+                DBG("Editor sees Hit #" << i
+                    << " classified as "
+                    << HitClassifier::toString(hit.type));
+            }
+>>>>>>> f3e6679 (button fixed)
+
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+HackBrownAudioProcessorEditor::HackBrownAudioProcessorEditor (HackBrownAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    recordButton.setClickingTogglesState(true);
+    recordButton.setToggleState(false, juce::dontSendNotification);
+    
+    recordButton.onClick = [&]() {
+        bool isOn = recordButton.getToggleState();
+        auto message = "Recording!";
+        if (!recordButton.getToggleState()) {
+            message = "Record";
+        }
+        //const auto message =  ? "Recording!" : "Record";
+        recordButton.setButtonText(message);
+        
+        if (!isOn && p.recordingStarted.load()) {
+            // algorithm classifies/do classification
+            p.inputProcessor.classifyStoredHits(p.getSampleRate());
+
+            DBG("---- Editor sees classified hits ----");
+            for (const auto& ch : p.inputProcessor.classifiedHits) //hits are stored in inputProcessor.classifiedhits
+            {
+                DBG("Hit index " << ch.hitIndex
+                    << " classified as "
+                    << HitClassifier::toString(ch.type));
+            }
+
+            recordButton.setButtonText("Recorded Thing");
+            // Call Gabe FFT Algorithm Here
+            // p.startRecording();
+            // Call Gabe FFT Algorithm Here  --> do classification instead
+            addAndMakeVisible(playButton);
         }
         
-        p.recordingEnabled.store(!isOn);
+        p.recordingEnabled.store(isOn);
     };
     
     playButton.onClick = [&]() {
         p.isPlaybackOn.store(true);
-        recordButton.setButtonText("Clicked!");
         
         std::cout << p.inputProcessor.storedHitsIndex << std::endl;
         //p.inputProcessor.storedHits;
@@ -55,7 +121,6 @@ HackBrownAudioProcessorEditor::HackBrownAudioProcessorEditor (HackBrownAudioProc
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     addAndMakeVisible(recordButton);
-    addAndMakeVisible(playButton);
     setSize (400, 300);
 }
 
